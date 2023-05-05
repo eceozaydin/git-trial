e8662b4999927c21858347d66fb49d63060d7726
eceozaydin <e.ozaydin@ebebek.com>
2023-05-05 14:30:31 +0300

    pull

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
         System.out.println("Hello world!");
         System.out.println("main");
-        System.out.println("main-deneme-2");
+        System.out.println("pullla gelecek");
     }
 }
